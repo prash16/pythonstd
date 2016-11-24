8efb54e092e8f60ee4d34ad0adec87df393247bf
Prash Medirattaa <prash16@gmail.com>
2016-11-23 16:32:45 -0800

    changes

---
+++ pythonstd/pythonstd.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+def standard_deviation(x):
+    n = len(x)
+    mean = sum(x) / n
+    ssq = sum((x_i-mean)**2 for x_i in x)
+    stdev = (ssq/n)**0.5
+    return(stdev)
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pythonstd',
+    version='0.1dev',
+    packages=['pythonstd',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
